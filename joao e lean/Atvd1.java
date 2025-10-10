8702231641b555079389f885f24a3d001dc9de73
Lchaves-01 <leonchaves26@gmail.com>
2025-10-10 08:18:11 -0300

    Add files via upload

---
+++ joao e lean/Atvd1.java
@@ -0,0 +1,18 @@
+
+public class Atvd1 {
+
+	public static void main(String[] args) {
+	double p,j,i;
+	
+	p = 145;
+	j = 134;
+	i = 1;
+	
+	while (j<p) {
+		j = j + 2.5;
+		p = p + 2;
+		i = i + 1;
+	}
+	System.out.println("Demorou " +i+ " anos" );
+	}
+}
